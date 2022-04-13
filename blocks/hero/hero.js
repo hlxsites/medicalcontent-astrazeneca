47fbe868f45338ea3adcef2dec65e6ea2ff9648e
rofe <github@rofe.com>
2022-04-13 11:58:20 +0200

    chore: shortcuts only for default content

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -13,7 +13,7 @@ export default function decorate(block) {
   // shortcut list
   const ul = document.createElement('ul');
   ul.className = 'hero-shortcuts';
-  document.querySelectorAll('main .default-content-wrapper > h2').forEach((item) => {
+  document.querySelectorAll('main > div:not(.related-container) .default-content-wrapper > h2').forEach((item) => {
     const li = document.createElement('li');
     li.innerHTML = `<a href="#${item.id}" title="${item.textContent}">${item.textContent}</a>`;
     ul.append(li);
