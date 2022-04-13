688c25ead361fa21b2cdb44663ae464546a7ff22
rofe <github@rofe.com>
2022-04-13 18:50:38 +0200

    feat: accordion

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,15 +1,23 @@
-export default function init(el) {
-  const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
-  titles.forEach((title) => {
+export default function decorate(block) {
+  block.querySelectorAll(':scope > div').forEach((segment) => {
+    // add a class to the segment
+    segment.classList.add('accordion-segment');
+    const title = segment.querySelector(':scope > div:first-child');
     // Add a class to the title container
-    title.classList.add('item-title');
-    // Remove the empty div
-    title.querySelector(':scope > div:last-of-type').remove();
-    // Add a class to the content
-    title.nextElementSibling.classList.add('item-content');
+    title.classList.add('accordion-item-title');
+    // Decorate the content divs
+    segment.querySelectorAll(':scope > div:not(:first-child')
+      .forEach((div) => {
+        div.classList.add('accordion-item-content');
+        div.querySelectorAll(':scope p').forEach((p) => {
+          if (p.children.length === 1 && p.firstChild.tagName === 'PICTURE') {
+            p.classList.add('accordion-image-only');
+          }
+        });
+      });
     // Add a click handler to open the content
     title.addEventListener('click', () => {
-      title.classList.toggle('open');
+      title.parentElement.classList.toggle('accordion-open');
     });
   });
 }
