004f42922e219efae428ad190a5fee675fc54c09
rofe <github@rofe.com>
2022-04-13 11:58:40 +0200

    feat: related block

---
+++ blocks/related/related.js
@@ -0,0 +1,26 @@
+export default function decorate(block) {
+  block.classList.add('cards');
+  const classes = [
+    'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+  ];
+  block.querySelectorAll(':scope > div > div').forEach((card, index) => {
+    const style = classes[index] || classes[index % classes.length];
+    card.classList.add('cards-card', `cards-card-${style}`);
+
+    const viewContent = document.createElement('p');
+    viewContent.classList.add('related-view-content');
+    card.append(viewContent);
+
+    const link = card.querySelector(':scope a');
+    if (link && link.href) {
+      const cardLink = document.createElement('a');
+      cardLink.href = link.href;
+      cardLink.title = link.title || link.textContent;
+      link.parentElement.append(...link.childNodes);
+      link.remove();
+      cardLink.append(...card.childNodes);
+      card.append(cardLink);
+    }
+    card.parentElement.replaceWith(card);
+  });
+}
