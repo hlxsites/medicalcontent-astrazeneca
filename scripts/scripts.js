6839439b75003a9132874899e6201accaf8eddab
Erhan Abibula <abibula@adobe.com>
2022-05-30 17:23:46 +0300

    move hero to main head.html & try parallel downloads

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -322,12 +322,30 @@ export async function loadBlock(block, eager = false) {
 export async function loadBlocks(main) {
   updateSectionsStatus(main);
   const blocks = [...main.querySelectorAll('div.block')];
+  const promises = [];
   for (let i = 0; i < blocks.length; i += 1) {
     // eslint-disable-next-line no-await-in-loop
-    await loadBlock(blocks[i]);
-    updateSectionsStatus(main);
+    // await loadBlock(blocks[i]);
+    promises.push(loadBlock(blocks[i]));
   }
+  await Promise.all(promises);
+  updateSectionsStatus(main);
 }
+
+/**
+ * Loads JS and CSS for all blocks in a container element.
+ * @param {Element} main The container element
+ */
+// export async function loadBlocks(main) {
+//   updateSectionsStatus(main);
+//   const blocks = [...main.querySelectorAll('div.block')];
+//   for (let i = 0; i < blocks.length; i += 1) {
+//     // eslint-disable-next-line no-await-in-loop
+//     await loadBlock(blocks[i]);
+//     updateSectionsStatus(main);
+//   }
+// }
+
 
 /**
  * Returns a picture element with webp and fallbacks
