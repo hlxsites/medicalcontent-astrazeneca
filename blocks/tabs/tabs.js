7155aa851d7207a41392f17777a8f67be85fbc3f
Erhan Abibula <abibula@adobe.com>
2022-05-31 13:33:15 +0300

    work on tabs & some small refactorings

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,21 +1,84 @@
+import { loadBlock } from "/scripts/scripts.js";
+
 /**
- * @param {HTMLDivElement} block 
+ * @param {HTMLDivElement} block
+ */
+function decorateBlock(block) {
+  block.classList.add("references-container");
+}
+
+function decorateTabs(block) {
+  const tabs = [...block.querySelectorAll(":scope > div")];
+  tabs.forEach((tab, idx) => {
+    tab.classList.add("tabs-tab");
+    if (idx > 0) {
+      tab.classList.add("tabs-tab--hidden");
+    }
+    tab.dataset.tab = idx + 1;
+
+    tab
+      .querySelector(":scope > div:nth-child(2)")
+      .classList.add("tabs-tab-content");
+    tab.querySelector(":scope > div:last-child").classList.add("references");
+  });
+}
+
+/**
+ * @param {HTMLDivElement} block
  */
 function createTabsMenu(block) {
-  const tabs = [...block.querySelectorAll(":scope > div")];
+  const tabs = [...block.querySelectorAll(":scope > div.tabs-tab")];
   const tabsBtn = tabs.map((tab) => tab.querySelector(":scope > div"));
 
   const tabsMenu = document.createElement("div");
   tabsMenu.className = "tabs-menu";
-  tabsBtn.forEach((tab) => {
+  tabsBtn.forEach((tab, idx) => {
     const btn = document.createElement("button");
-    btn.className = "tabs-menu-button";
+    btn.classList.add("tabs-menu-button");
+    if (idx === 0) {
+      btn.classList.add("tabs-menu-button--active");
+    }
     btn.innerText = tab.innerText;
+    btn.dataset.tab = idx + 1;
     tabsMenu.appendChild(btn);
   });
   block.prepend(tabsMenu);
 }
 
-export default async function decorateTabs(block, blockName, doc, isEager) {
+function initEvents(block) {
+  document.querySelectorAll("button.tabs-menu-button").forEach((button) => {
+    button.addEventListener("click", () => {
+      button.parentElement
+        .querySelectorAll("button.tabs-menu-button")
+        .forEach((button) =>
+          button.classList.remove("tabs-menu-button--active")
+        );
+      button.classList.add("tabs-menu-button--active");
+      const tabId = button.dataset.tab;
+      block.querySelectorAll("div.tabs-tab").forEach((tab) => {
+        tab.classList.add("tabs-tab--hidden");
+        if (tab.dataset.tab === tabId) {
+          tab.classList.remove("tabs-tab--hidden");
+        }
+      });
+    });
+  });
+}
+
+/**
+ * load the references block if not loaded
+ * @param {HTMLDivElement} block
+ */
+async function loadReferences(block) {
+  const referencesBlock = block.querySelector(".references");
+  referencesBlock.dataset.blockName = "references";
+  return loadBlock(referencesBlock);
+}
+
+export default async function initializeTabs(block, blockName, doc, isEager) {
+  decorateBlock(block);
+  decorateTabs(block);
   createTabsMenu(block);
+  initEvents(block);
+  return await loadReferences(block);
 }
